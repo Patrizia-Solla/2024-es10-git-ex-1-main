b75bf22f5060687cd303b6c4cffb0daaf2712cbe
Patrizia-Solla <patrizia.solla@outlook.it>
2024-06-25 14:01:49 +0200

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        test();
+    }
+
+    public static void test() {
+        System.out.println("Hello World!");
+    }
+}
